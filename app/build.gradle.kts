a3c986e494519b44bfecb7c19b5b76ca261a5852
agryogim3328 <yogi.muhammad@ai.astra.co.id>
2025-01-09 21:06:37 +0700

    added publication

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,10 +43,9 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
-    implementation(libs.tanya.library)
+//    implementation(libs.tanya.library)
 
     implementation(libs.androidx.constraintlayout)
-    implementation(project(":tanya-bestie"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,4 +22,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "LibSample"
 include(":app")
-include(":tanya-bestie")
